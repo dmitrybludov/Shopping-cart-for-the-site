27ecd06b2400fd609eb76b5e424ce754736c4a0e
dmitrybludov <bludovdmitry@gmail.com>
2016-05-25 13:51:26 +0300

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+
+	$(".header a, a.top").mPageScroll2id({
+		offset : 130
+	});
+})
---
+++ js/waypoints.js
@@ -0,0 +1,11 @@
+//var $works = $('.works');
+
+//$works.waypoint(function () {
+//	$works.addClass('js-media-animate');
+//}, { offset: '50%'});
+
+//$('.media').waypoint( function() {
+//	$(this).addClass('animated bounceInUp');
+//}, {
+//	offset: "70%"
+//});
